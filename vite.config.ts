281745b90667146e3d3e46a2b640d1a0a74c492c
Bayu Maulana <bayufransdo@gmail.com>
2025-07-28 16:01:26 +0700

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,       
-    port: 5173,        
+export default defineConfig(({ mode }) => {
+  let outDir = 'dist' // default
+
+  if (mode === 'prod') {
+    outDir = '/var/www/osc-2025'
+  }
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true,
+      port: 5173,
+    },
+    build: {
+      outDir,
+    },
   }
 })
