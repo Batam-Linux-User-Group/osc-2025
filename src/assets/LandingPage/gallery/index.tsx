06d386fc87cbbd89a9f109d2e487e0e76a11e125
Bayu Maulana <bayufransdo@gmail.com>
2025-07-30 18:54:11 +0700

    fix build error on linux

--- src/assets/LandingPage/gallery/index.tsx
+++ src/assets/LandingPage/gallery/index.tsx
@@ -1,5 +1,5 @@
-import satu from './satu.jpg';
-import dua from './dua.jpg';
+import satu from './satu.webp';
+import dua from './dua.webp';
 import tiga from './tiga.png';
 import empat from './empat.png';
 import lima from './lima.png';
